64e4f49a8d21eda3d3c0255be9526a9169a60745
Samiyaxoxo <Samiyamohammadi03@gmail.com>
2025-03-11 22:33:38 +0530

    square sum.py

---
+++ square sum.py
@@ -0,0 +1,5 @@
+def sum_of_squares(n):
+    return (n * (n + 1) * (2 * n + 1))
+num = int(input("Enter a number: "))
+print(f"Sum of squares of first {num} numbers is {sum_of_squares(num)}")
+print("samiya,23")
