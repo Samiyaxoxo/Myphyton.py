07d2d339c4d04c09f2faa6006fd5196fe4bb4e25
Samiyaxoxo <Samiyamohammadi03@gmail.com>
2025-03-11 22:44:24 +0530

    Grade.py

---
+++ Grade.py
@@ -0,0 +1,10 @@
+a=int(input("enter the marks"))
+if(a>=90):
+  print("grade,A")
+elif(a<=89 and a>=75):
+    print("grade,B")
+elif(a<=74 and a>=50):
+      print("grade,C")
+else:
+        print("grade,F")
+        print("samiya,23")
