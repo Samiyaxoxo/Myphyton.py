222467e986c1fb93db85f7a071276d5c8b845c9e
Samiyaxoxo <Samiyamohammadi03@gmail.com>
2025-03-11 23:51:13 +0530

    Create average.py

---
+++ average.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+average = (num1 + num2 + num3) / 3
+
+print(f"The average of {num1}, {num2}, and {num3} is: {average:.2f}")
+print("samiya,23")
