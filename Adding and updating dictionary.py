2e6feb71fd09c5db35d190f8bea499a51512a917
Samiyaxoxo <Samiyamohammadi03@gmail.com>
2025-03-13 15:31:29 +0530

    Create Adding and updating dictionary.py

---
+++ Adding and updating dictionary.py
@@ -0,0 +1,9 @@
+lana_del_rey = {
+    "sad":"summer time sadness",
+    "peace":"cinnamon girl",
+    "contemplating":"white mustang",
+}   
+lana_del_rey["satisfying"]="ultraviolence"
+print(lana_del_rey)
+lana_del_rey["contemplating"] = "brooklyn baby"
+print(lana_del_rey)
