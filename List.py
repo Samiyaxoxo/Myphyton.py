1b0cfc8cde632354b4cce95193588b2b7cdc2d21
Samiyaxoxo <Samiyamohammadi03@gmail.com>
2025-03-13 11:01:57 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,16 @@
+samiya=[5,3,7,9]
+samiya[2]
+samiya
+samiya[3]=6
+samiya 
+samiya.append(4)
+samiya
+samiya.sort()
+samiya
+samiya.insert(2,8)
+samiya
+samiya.remove(7)
+samiya
+samiya.pop(3)
+samiya
+           
